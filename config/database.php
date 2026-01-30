009d45f234d47b1e37862b0b9f4801b32e818fdd
Kenway23 <rifkimuhamad6635@gmail.com>
2026-01-30 14:10:46 +0700

    update

--- config/database.php
+++ config/database.php
@@ -1,12 +1,20 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "midnightplay";
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$host = getenv("MYSQLHOST");
+$port = getenv("MYSQLPORT");
+$dbname = getenv("MYSQLDATABASE");
+$username = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
 
-if (!$conn) {
-    die("Koneksi database gagal!");
+try {
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
+
+    $pdo = new PDO($dsn, $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_TIMEOUT => 5
+    ]);
+} catch (PDOException $e) {
+    // JANGAN echo di production
+    error_log($e->getMessage());
+    exit;
 }
-?>
